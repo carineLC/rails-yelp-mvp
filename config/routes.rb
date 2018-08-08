69b10302b301237b4816d20cc135a2aef198b5ae
Carine LE CHARLES <carine.le.charles@gmail.com>
2018-08-08 14:23:21 +0200

    Routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants do
+    member do
+      resources :reviews, only: [:index, :new, :create]
+    end
+  end
 end
